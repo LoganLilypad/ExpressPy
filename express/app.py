fceb8fd55a7298b87d93ad5d8233b0246f25c5c3
Recal <recalibratedgh@gmail.com>
2020-09-19 16:56:19 +0000

    Make sure port is not in use before attempting to serve.

--- express/app.py
+++ express/app.py
@@ -2,6 +2,8 @@ from express.routes.handler import RouteHandler
 from express.routes.router import Router
 from express.exceptions.errors import AppError
 from express.tcpserver.server import Server
+import socket 
+
 
 class App:
   
@@ -15,12 +17,20 @@ class App:
   def handler(self) -> RouteHandler:
     return self._handler
 
+  def port_in_use(self, port: int) -> bool:
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    return not bool(sock.connect_ex( ('localhost', port) ))
+
+  def is_valid_port(self, port: int) -> bool:
+      return type(port) == int or port > 0 or port < 65536
+
   def listen(self, port: int = 3000) -> None:
-    if type(port) != int:
+    if not self.is_valid_port(port):
       raise AppError("Mismatch: Type '{}' passed when int was expected.".format(type(port)))
       
-    self._server = Server(port)
-    
-    print("Listening on localhost:{}.".format(port))
-
-    self._server.start()
+    if not self.port_in_use(port):
+        self._server = Server(port)
+        print("Listening on localhost:{}.".format(port))
+        self._server.start()
+    else:
+        raise AppError("Port '{}' is already in use.".format(port))
