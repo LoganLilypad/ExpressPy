6d4d2efa48ba6d74042894a9fd3fec266f766cf8
Recal <recalibratedgh@gmail.com>
2020-10-28 20:54:13 +0000

    Fix this junk

--- express/tcpserver/socket_thread.py
+++ express/tcpserver/socket_thread.py
@@ -3,6 +3,7 @@ import socket, os, errno
 from time import sleep
 from express.request import Request
 from express.response import Response
+import keyboard
 
 class ServerSocketThread(Thread):
 
@@ -27,6 +28,21 @@ class ServerSocketThread(Thread):
             sock.close
             exit()
 
+    def _handle_request(self, conn, addr) -> None:
+        while True:
+            data = conn.recv(2048)
+
+            if not data:
+                break
+            else:
+                conn.send(bytes(data))
+                conn.close()
+
+    def valid_exit(self) -> bool:
+        # Hacky method but who fucking cares? not me
+        return keyboard.is_pressed("ctrl+c") or keyboard.is_pressed("ctrl+d") or keyboard.is_pressed("ctrl+z")
+
+
     def run(self) -> None:
         
       # Should probably give this a more descriptive name but that's alright I guess.
@@ -34,40 +50,29 @@ class ServerSocketThread(Thread):
       serv = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
       serv.bind( ( self.host, self.port ) ) # Bind on localhost:{port}
       serv.setblocking(0)
+      serv.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
       serv.settimeout(self.timeout)
       self.socket = serv
 
       # fcntl.fcntl(serv, fcntl.F_SETFL, os.O_NONBLOCK)
       # Realized the above only works on linux, which is pretty annoying but alright.
 
-      serv.listen(1)
+      serv.listen(1) # No clue.
 
-      
+      while True:
+        try:
+            if self.valid_exit():
+                break
 
-      try:
-          while True:
-              try:
-                  conn, addr = serv.accept()
-                  data = conn.recv(8192).decode().strip()
+            conn, addr = serv.accept()
 
-                  if len(data) > 0:
-                      req = Request.parse(Request.decode(data))
+            active_thread = Thread(target=self._handle_request, args=( conn, addr ) )
 
-                      Handler = self.app.handler()
-
-                      if Handler.is_accepted(req.headers["path"], req.headers["path"]):
-                          Handler.get(req.headers["path"]).handle(req, Response(conn))
-                      else:
-                          pass
-                  else:
-                      pass
-              except socket.timeout:
-                  pass # Unsure what to do here.
-              except KeyboardInterrupt:
-                  pass
-      except KeyboardInterrupt:
-          exit()
-
+            active_thread.daemon = True
+            active_thread.start()
+        except:
+            pass
+    
 
       #while True:
       #  try:
